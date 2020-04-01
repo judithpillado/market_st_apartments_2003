5be912c336b6167c4cd5fc28ca579881f3adf48c
Judith Pillado <judith.pillado@gmail.com>
2020-04-01 11:29:48 -0500

    Fixed test for addition of renters

--- test/building_test.rb
+++ test/building_test.rb
@@ -38,22 +38,11 @@ class BuildingTest < Minitest::Test
     unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
     unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
     renter1 = Renter.new("Aurora")
+    building.add_unit(unit1)
 
     unit1.add_renter(renter1)
 
     assert_equal ["Aurora"], building.renters
-  end
-
-  def test_addition_of_renter2
-    building = Building.new
-    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
-    unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
-    renter1 = Renter.new("Aurora")
-    renter2 = Renter.new("Tim")
-
-    unit2.add_renter(renter2)
-
-    assert_equal ["Tim"], building.renters
   end
 
   def test_building_renters
@@ -62,6 +51,10 @@ class BuildingTest < Minitest::Test
     unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
     renter1 = Renter.new("Aurora")
     renter2 = Renter.new("Tim")
+    building.add_unit(unit1)
+    building.add_unit(unit2)
+    unit1.add_renter(renter1)
+    unit2.add_renter(renter2)
 
     assert_equal ["Aurora", "Tim"], building.renters
   end
@@ -70,8 +63,8 @@ class BuildingTest < Minitest::Test
     building = Building.new
     unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
     unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
-    renter1 = Renter.new("Aurora")
-    renter2 = Renter.new("Tim")
+    building.add_unit(unit1)
+    building.add_unit(unit2)
 
     assert_equal 1099.5, building.average_rent
   end
