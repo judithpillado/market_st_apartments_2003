058ec1928f2e169a6dc7d86db6c19c296d572be4
Judith Pillado <judith.pillado@gmail.com>
2020-04-01 10:43:03 -0500

    Add renters empty array

--- lib/building.rb
+++ lib/building.rb
@@ -1,7 +1,12 @@
 class Building
-  attr_reader :units
+  attr_reader :units, :renters
 
   def initialize
     @units = []
+    @renters = []
+  end
+
+  def add_unit(building_unit)
+    @units << building_unit
   end
 end
