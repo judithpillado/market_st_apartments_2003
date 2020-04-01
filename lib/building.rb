cb0b007d4ee62edba9acd581af9855f7ea063800
Judith Pillado <judith.pillado@gmail.com>
2020-04-01 11:46:50 -0500

    Added test for rented units

--- lib/building.rb
+++ lib/building.rb
@@ -1,9 +1,10 @@
 class Building
-  attr_reader :units, :renters
+  attr_reader :units, :renters, :rented_units
 
   def initialize
     @units = []
     @renters = []
+    @rented_units = []
   end
 
   def add_unit(building_unit)
@@ -19,14 +20,16 @@ class Building
   @renters
 end
 
-  # def add_renters_to_units
-  #   @units << @renters
-  # end
-  #
-  # def add_renter(renters)
-  # @renters << (renters)
-  # end
   def average_rent
-    @units.sum(&:monthly_rent) / 2.0
+    @units.sum(&:monthly_rent) / @units.count.to_f
   end
+
+  def rented_units
+    @rented_units = []
+    @units.each do |unit|
+      unit.renter != nil
+      @rented_units << unit
+    end
+  @rented_units
+end
 end
