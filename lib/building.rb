209bcb67a836aecbd28c76b70a6dd725f5edc5fa
Judith Pillado <judith.pillado@gmail.com>
2020-04-01 11:01:40 -0500

    Add method for average rent

--- lib/building.rb
+++ lib/building.rb
@@ -9,4 +9,12 @@ class Building
   def add_unit(building_unit)
     @units << building_unit
   end
+
+  def add_renter(renters)
+  @renters << (renter1)
+  end
+
+  def building_average_rent
+    @units.sum(&:monthly_rent) / 2.0
+  end
 end
